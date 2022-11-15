cd6988e57430b1e7b2a03a82e4d2a441e2172852
Dmytro Plechun <ifunnd@gmail.com>
2022-11-15 04:03:58 +0200

    doctors added

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,48 @@
+class DoctorsController < ApplicationController
+  def index
+    @doctors = Doctor.all
+  end
+
+  def show
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save
+      redirect_to @doctor
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def update
+    @doctor = Doctor.find(params[:id])
+    if @doctor.update(doctor_params)
+      redirect_to @doctor
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @doctor = Doctor.find(params[:id])
+    @doctor.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:first_name, :last_name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   end
   resources :services
   resources :medicines
+  resources :doctors
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
